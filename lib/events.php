1d16173183aac492aeef11b11e9e4abfb613843d
Jerome Bakker <jeabakker@coldtrick.com>
2012-06-07 11:10:13 +0200

    fixed: old link in notification

--- lib/events.php
+++ lib/events.php
@@ -7,7 +7,7 @@
 			$user_one = get_user($object->guid_one);
 			$user_two = get_user($object->guid_two);
 				
-			$view_friends_url = elgg_get_site_url() . "pg/friend_request/" . $user_two->username;
+			$view_friends_url = elgg_get_site_url() . "friend_request/" . $user_two->username;
 				
 			// Notify target user
 			$subject = elgg_echo('friend_request:newfriend:subject', array($user_one->name));
