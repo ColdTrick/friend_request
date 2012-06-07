a4029a0bc8519bf505f11c3260f7e634e64c4f88
Jerome Bakker <jeabakker@coldtrick.com>
2012-06-07 11:10:47 +0200

    changed: small code optimalizations

--- lib/hooks.php
+++ lib/hooks.php
@@ -4,14 +4,17 @@
 		$result = $returnvalue;
 		
 		if(!empty($params) && is_array($params) && ($user = elgg_get_logged_in_user_entity())){
-			$entity = $params["entity"];
+			$entity = elgg_extract("entity", $params);
 			
 			if(elgg_instanceof($entity, "user") && ($entity->getGUID() != $user->getGUID())){
 				if(check_entity_relationship($user->getGUID(), "friendrequest", $entity->getGUID())){
 					foreach($result as &$item){
+						// change the text of the button to tell you already requested a friendship
 						if($item->getName() == "add_friend"){
 							$item->setText(elgg_echo("friend_request:friend:add:pending"));
 							$item->setHref("friend_request/" . $user->username . "#friend_request_sent_listing");
+							
+							break;
 						}
 					}
 				}
